2ab08c0614644c1365b9bfa2e812c913226370a6
Ricardo Miralles <ricmiber@gmail.com>
2024-02-08 17:21:44 +0100

    Add: solution from part08 exercises to 8.09

---
+++ apps/part08/library-frontend-graphql/src/components/Authors.jsx
@@ -0,0 +1,54 @@
+import { gql, useQuery } from '@apollo/client';
+import React from 'react';
+
+export default function Authors(props) {
+    
+    if (!props.show) {
+        return null
+      }
+      const authors = []
+
+      const ALL_AUTHORS = gql`
+        query {
+            allAuthors {
+            name
+            born
+            bookCount
+            }
+        }
+        `
+
+    const result = useQuery(ALL_AUTHORS)
+    if (result.loading)  {
+        return <div>loading...</div>
+    }
+    if (!result.data) {
+        return <div>no data</div>
+    }
+    else {
+        authors.push(...result.data.allAuthors)
+        console.log(authors)
+    }
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {authors.map((a) => (
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
---
+++ apps/part08/library-frontend-graphql/src/components/Books.jsx
@@ -0,0 +1,56 @@
+import { gql, useQuery } from '@apollo/client';
+import React from 'react';
+
+export default function Books(props) {
+
+    if (!props.show) {
+        return null
+      }
+
+      const books = []
+      const ALL_BOOKS = gql`
+        query {
+            allBooks {
+            title
+            author
+            published
+            }
+        }
+        `
+        const result = useQuery(ALL_BOOKS)
+        if (result.loading)  {
+            return <div>loading...</div>
+        }
+        if (!result.data) {
+            return <div>no data</div>
+        }
+        else {
+            books.push(...result.data.allBooks)
+            console.log(books)
+        }
+
+  
+
+  return (
+    <div>
+      <h2>books</h2>
+
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>author</th>
+            <th>published</th>
+          </tr>
+          {books.map((a) => (
+            <tr key={a.title}>
+              <td style={{textAlign: 'left'}}>{a.title}</td>
+              <td>{a.author}</td>
+              <td>{a.published}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
