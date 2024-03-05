e9979e5eb6f12fffad3f37b545f072ebe1d0b00a
Ricardo Miralles <ricmiber@gmail.com>
2024-03-05 12:42:57 +0100

    Add: solution from part09 exercises to 9.03

---
+++ apps/part09/exercises_9.01-9.07/bmiCalculator.ts
@@ -0,0 +1,42 @@
+interface MultiplyValues
+{
+    value1: number,
+    value2: number
+}
+
+const parseArguments = (args: string[]): MultiplyValues => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    if (args.length > 4) throw new Error('Too many arguments');
+
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        return {
+            value1: Number(args[2]),
+            value2: Number(args[3])
+        }
+    } else {
+        throw new Error('Provided values were not numbers!');
+    }
+}
+
+const bmiCalculator = (height: number, weight: number): string => {
+    const bmi = weight / Math.pow(height / 100, 2);
+    if (bmi < 18.5) {
+        return 'Underweight';
+    } else if (bmi >= 18.5 && bmi < 25) {
+        return 'Normal (healthy weight)';
+    } else if (bmi >= 25 && bmi < 30) {
+        return 'Overweight';
+    } else {
+        return 'Obese';
+    }
+}
+
+try {
+    const { value1, value2 } = parseArguments(process.argv);
+    console.log(bmiCalculator(value1, value2));
+}catch (e: unknown) {
+    let errorMessage = e as Error;
+    console.log('Error, something bad happened, message: ', errorMessage.message);
+}
+
+// console.log(bmiCalculator(180,74)); // Normal (healthy weight)
---
+++ apps/part09/exercises_9.01-9.07/exerciseCalculator.ts
@@ -0,0 +1,75 @@
+interface WeeklyGoal
+{
+    periodLength: number,
+    trainingDays: number,
+    success: boolean,
+    rating: number,
+    ratingDescription: string,
+    target: number,
+    average: number
+}
+
+interface CommandArgs
+{
+    dailyExercises: number[],
+    target: number
+}
+
+const parseArgs= (args: string[]): CommandArgs => {
+    if (args.length < 7) throw new Error('Not enough arguments');
+    if (args.slice(2).every(a => !isNaN(Number(a)))) {
+        return {
+            dailyExercises: args.slice(2, -1).map(a => Number(a)),
+            target: Number(args[args.length - 1])
+        }
+    } else {
+        throw new Error('Provided values were not numbers!');
+    }
+}
+
+
+const calculateExercises = (dailyExercises: number[] , target: number):WeeklyGoal => {
+    const average = dailyExercises.reduce((a, b) => a + b, 0) / dailyExercises.length;
+    console.log(dailyExercises);
+    const ratingDescription = (average: number, target: number): string => {
+        if (average < target) {
+            return 'not too bad but could be better';
+        } else if (average === target) {
+            return 'good job';
+        } else {
+            return 'excellent';
+        }
+    }
+
+    const rates = (average: number, target: number): number => {
+        const myRating = average / target;
+        if (myRating < 0.5) {
+            return 1;
+        } else if (myRating >= 0.5 && myRating < 1) {
+            return 2;
+        } else {
+            return 3;
+        }
+    }
+
+    const ratingText = ratingDescription(dailyExercises.reduce((a, b) => a + b, 0) / dailyExercises.length, target)
+    const rating =  rates(average, target)
+    return {
+        periodLength: dailyExercises.length,
+        trainingDays: dailyExercises.filter(d => d > 0).length,
+        success: dailyExercises.every(d => d >= target),
+        rating,
+        ratingDescription: ratingText,
+        target,
+        average
+    }
+ }
+
+ try {
+    const { dailyExercises, target } = parseArgs(process.argv);
+    console.log(calculateExercises(dailyExercises, target));
+ }catch (e: unknown) {
+    let errorMessage = e as Error;
+    console.log('Error, something bad happened, message: ', errorMessage.message);
+ }
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2)); // { periodLength: 7, trainingDays: 5, success: false, rating: 3, ratingDescription: 'good job', target: 2, average: 2.0714285714285716 }
